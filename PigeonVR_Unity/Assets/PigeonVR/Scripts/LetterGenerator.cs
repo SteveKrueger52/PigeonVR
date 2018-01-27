685036c6e7351d658c983d13aa14a67228784625
GerritsC <gerrits.c@husky.neu.edu>
2018-01-27 15:06:10 -0500

    Letter generation has BEGUN

---
+++ PigeonVR_Unity/Assets/PigeonVR/Scripts/LetterGenerator.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LetterGenerator : MonoBehaviour {
+
+    [SerializeField]
+    GameObject[] spawnpoints;
+
+    [SerializeField]
+    float spawnDelay;
+
+    [SerializeField]
+    GameObject[] LetterTypes;
+
+
+    // Use this for initialization
+    void Start () {
+        StartCoroutine("spawnLetters");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public IEnumerator spawnLetters()
+    {
+
+        yield return new WaitForSeconds(spawnDelay);
+    }
+}
