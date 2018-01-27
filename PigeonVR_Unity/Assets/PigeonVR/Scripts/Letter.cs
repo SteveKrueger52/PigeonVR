4eb582af0ed9664926bbe8b5aca4873811f2e6b3
Ameen Radwan <therespie@gmail.com>
2018-01-27 17:08:05 -0500

    text style fixes

--- PigeonVR_Unity/Assets/PigeonVR/Scripts/Letter.cs
+++ PigeonVR_Unity/Assets/PigeonVR/Scripts/Letter.cs
@@ -18,24 +18,24 @@ public class Letter : MonoBehaviour
     {
         letterID = Info.getRandomLocation();
 
-       Canvas canvas = this.gameObject.AddComponent<Canvas>();
-      canvas.renderMode = RenderMode.WorldSpace;
-      GameObject go = new GameObject("LetterText");
-      go.transform.SetParent(canvas.transform);
-       displayText = go.AddComponent<Text>();
+        Canvas canvas = this.gameObject.AddComponent<Canvas>();
+        canvas.renderMode = RenderMode.WorldSpace;
+        GameObject go = new GameObject("LetterText");
+        go.transform.SetParent(canvas.transform);
+        displayText = go.AddComponent<Text>();
 
-      Font morris = Resources.Load<Font>("MorrisRoman-Black");
+        Font morris = Resources.Load<Font>("MorrisRoman-Black");
+        displayText.font = morris;
 
-      displayText.font = morris;
-      displayText.transform.localScale = new Vector3(0.05f, 0.05f, 1);
-      displayText.transform.localPosition = new Vector3(0, 0, 0);
-      displayText.alignment = TextAnchor.MiddleCenter;
-      setStartData(letterID);
-   }
+        displayText.transform.localScale = new Vector3(0.05f, 0.05f, 1);
+        displayText.transform.localPosition = new Vector3(0, 0, 0);
+        displayText.alignment = TextAnchor.UpperCenter;
+        setStartData(letterID);
+    }
 
-   void setStartData(Info.Location id)
+    void setStartData(Info.Location id)
     {
         this.displayDestination = Info.getRandomSender(id);
-       displayText.text = displayDestination;
-   }
+        displayText.text = displayDestination;
+    }
 }
