45f1baa8c28ad6a32b2d1f66abd921e29f6fff73
cpasyanos <pasyanos.c@husky.neu.edu>
2018-01-27 15:06:03 -0500

    Why is visual studio crashing Unity

--- PigeonVR_Unity/Assets/PigeonVR/Scripts/ScoreSetter.cs
+++ PigeonVR_Unity/Assets/PigeonVR/Scripts/ScoreSetter.cs
@@ -4,18 +4,56 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class ScoreSetter : MonoBehaviour {
-   private int score = 0;
 
-   // [SerializeField]
    private Text scoreText;
+
+   [SerializeField]
+   private Image life1;
+
+   [SerializeField]
+   private Image life2;
+
+   [SerializeField]
+   private Image life3;
+
+   void OnEnable()
+   {
+      StateManager.Instance.onStateUpdate += changeScoreNLives;
+   }
+
+   void OnDisable()
+   {
+      StateManager.Instance.onStateUpdate -= changeScoreNLives;
+   }
 
 	// Use this for initialization
 	void Start () {
+      life1.gameObject.SetActive(true);
+      life2.gameObject.SetActive(true);
+      life3.gameObject.SetActive(true);
+
       scoreText = gameObject.GetComponent<Text>();
+      scoreText.text = "Score: 0";
 	}
 	
-	// Update is called once per frame
-	void Update () {
+   private void changeScoreNLives(int score, int lives)
+   {
       scoreText.text = "Score: " + score;
-	}
+
+      switch(lives)
+      {
+         case 2:
+            life3.gameObject.SetActive(false);
+            break;
+         case 1:
+            life2.gameObject.SetActive(false);
+            break;
+         case 0:
+            life1.gameObject.SetActive(false);
+            //Debug.Log("Game Over");
+            break;
+         default: // lives = 3, do nothing
+            break;
+      }
+   }
 }
