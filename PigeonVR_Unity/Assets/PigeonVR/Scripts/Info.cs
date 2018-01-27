9a4e751ad37df11933b2b0cb140444a0e243b79f
Ameen Radwan <therespie@gmail.com>
2018-01-26 22:37:58 -0500

    basic location data loader

--- PigeonVR_Unity/Assets/PigeonVR/Scripts/Info.cs
+++ PigeonVR_Unity/Assets/PigeonVR/Scripts/Info.cs
@@ -4,14 +4,53 @@ using System.IO;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public static class Info : MonoBehaviour{
+public class Info : MonoBehaviour{
     // Location Enumeration
     public enum Location { TOWN, FOREST, DESERT, SNOW, VOLCANO, WASTELAND, CITY, MOON}
+
+    [System.Serializable]
+    public struct LocationDataWrapper {
+        public LocationData[] locations;
+
+        public override string ToString() {
+            string res = "locations: [";
+            foreach(var l in locations) {
+                res += "{" + l.ToString() + "}";
+            }
+            res += "]";
+            return res;
+        }
+    }
+
+    [System.Serializable]
+    public struct LocationData {
+        public string id;
+        public LetterData[] letters;
+
+        public override string ToString() {
+            string res = "id: " + id + ", letters: [";
+            foreach(var letter in letters) {
+                res += "{" + letter.ToString() + "}";
+            }
+            res += "]";
+            return res;
+        }
+    }
+
+    [System.Serializable]
+    public struct LetterData {
+        public string text;
+        public int weight;
+
+        public override string ToString() {
+            return "text: " + text + ", weight: " + weight;
+        }
+    }
 
     // Singleton Code
     public static Info instance = null;
     private static string gameDataFileName = "data.json";
-    private Dictionary <Location, string[]> _senders = new Dictionary<Location, string> ();
+    private Dictionary <Location, string[]> _senders = new Dictionary<Location, string[]> ();
 
     private void Awake() {
         // setup of Singleton
@@ -31,7 +70,8 @@ public static class Info : MonoBehaviour{
             // Read the json from the file into a string
             string dataAsJson = File.ReadAllText(filepath); 
             // Pass the json to JsonUtility, and tell it to create a GameData object from it
-            JsonUtility.FromJson<Info>(dataAsJson);
+            LocationDataWrapper locationData = JsonUtility.FromJson<LocationDataWrapper>(dataAsJson);
+            Debug.Log(locationData);
         }
         else
         {
@@ -50,7 +90,7 @@ public static class Info : MonoBehaviour{
     // Gets a list of all senders
     public static string[] getAllSenders (Location loc) {
         string[] output;
-        if (instance._senders.TryGetValue(loc,output))
+        if (instance._senders.TryGetValue(loc, out output))
             return output;
         return new string[]{};
     }
@@ -58,7 +98,7 @@ public static class Info : MonoBehaviour{
     // Gets a random sender
     public static string getRandomSender (Location loc) {
         string[] output;
-        if (instance._senders.TryGetValue(loc,output)) {
+        if (instance._senders.TryGetValue(loc, out output)) {
             return output [(int) Random.Range (0, output.GetLength (0) - 1)];
         }
         return "Nobody In Particular";
