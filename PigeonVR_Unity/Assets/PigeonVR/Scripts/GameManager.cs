916aed90313d80863c9c7a97cdeabb498ff5fd5f
Ameen Radwan <therespie@gmail.com>
2018-01-27 11:56:02 -0500

    created basic state manager and score test script

---
+++ PigeonVR_Unity/Assets/PigeonVR/Scripts/GameManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    public static GameManager instance = null;
+    private static int score;
+    private int lives;
+
+    public static int Score
+    {
+        get
+        {
+            return score;
+        }
+    }
+
+    void Awake()
+    {
+        // setup of Singleton
+        if (instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        instance = this;
+
+        score = 0;
+        lives = 3;
+
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    public static GameManager Instance
+    {
+        get
+        {
+            return instance ?? (instance = new GameObject("GameManager").AddComponent<GameManager>());
+        }
+    }
+
+    public static void updateScore(int i)
+    {
+        score += i;
+    }
+}
---
+++ PigeonVR_Unity/Assets/PigeonVR/Scripts/Score.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class Score : MonoBehaviour {
+
+	private Text display;
+
+	void Start()
+	{
+		display = GetComponent<Text>();
+	}
+	
+	void Update () {
+		display.text = "Score: " + GameManager.Score.ToString();
+		if (Input.GetKeyUp(KeyCode.Space))
+        {
+            GameManager.updateScore(1);
+        }
+	}
+}
