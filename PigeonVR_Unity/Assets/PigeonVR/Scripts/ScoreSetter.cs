969119954c8cb299e76f44124b73f7db1925ca69
cpasyanos <pasyanos.c@husky.neu.edu>
2018-01-27 12:25:14 -0500

    Added script for score display

---
+++ PigeonVR_Unity/Assets/PigeonVR/Scripts/ScoreSetter.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreSetter : MonoBehaviour {
+   private int score = 0;
+
+   // [SerializeField]
+   private Text scoreText;
+
+	// Use this for initialization
+	void Start () {
+      scoreText = gameObject.GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+      scoreText.text = "Score: " + score;
+	}
+}
