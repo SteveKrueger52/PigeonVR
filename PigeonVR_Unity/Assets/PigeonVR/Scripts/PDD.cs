59a35d1c2152aa072df9b3bed91061ecee5394f3
U-HEAVENFLASH\Uttkarsh <narayan.u@husky.neu.edu>
2018-01-27 18:57:06 -0500

    update pDD

--- PigeonVR_Unity/Assets/PigeonVR/Scripts/PDD.cs
+++ PigeonVR_Unity/Assets/PigeonVR/Scripts/PDD.cs
@@ -16,7 +16,7 @@ public class PDD : MonoBehaviour
     private Vector3 north;
 
     float angle;
-    public Vector3 forward =  Vector3.forward;
+    private string zone = "nothing here";
 
    // public RectTransform north_layer, pigeon_direction_layer;
 
@@ -51,40 +51,75 @@ public class PDD : MonoBehaviour
 
 
         if (city.position.x > pigeon_direction.x && desert.position.x < pigeon_direction.x)
-        { if (city.position.y > pigeon_direction.y && desert.position.y < pigeon_direction.y)
-            //score++ for desert
+        {
+            if (city.position.y > pigeon_direction.y && desert.position.y < pigeon_direction.y)
+            {
+                zone = "DESERT";
+                //score++ for desert
             }
+
+        }
         else if (snow.position.x > pigeon_direction.x && city.position.x < pigeon_direction.x)
-        { if (snow.position.y > pigeon_direction.y && city.position.y < pigeon_direction.y)
+        {
+            if (snow.position.y > pigeon_direction.y && city.position.y < pigeon_direction.y)
+            {
+                zone = "CITY";
                 //score++ for city
-                }
+            }
+
+        }
         else if(town.position.x >pigeon_direction.x && snow.position.x < pigeon_direction.x)
         {  
             if(town.position.y>pigeon_direction.y && snow.position.y < pigeon_direction.y)
+            {
+                zone = "SNOW";
                 //score++ for snow
+            }
+
         }
         else if(forest.position.x > pigeon_direction.x && town.position.x < pigeon_direction.x)
         {
             if(forest.position.y > pigeon_direction.y && town.position.y < pigeon_direction.y)
+            {
+                zone = "TOWN";
                 //score ++ for town
+            }
+
         }
         else if(volcano.position.x > pigeon_direction.x && forest.position.x < pigeon_direction.x)
         {
             if(volcano.position.y >pigeon_direction.y && forest.position.y < pigeon_direction.y)
+            {
+                zone = "FOREST";
                 //score ++ for forest
+            }
+
         }
         else if(wasteland.position.x > pigeon_direction.x && volcano.position.x < pigeon_direction.x)
         {
             if(wasteland.position.y > pigeon_direction.y && volcano.position.y < pigeon_direction.y)
+            {
+                zone = "VOLCANO";
                 //score ++ for volcano
+            }
+
         }
         else if(desert.position.x > pigeon_direction.x && wasteland.position.x < pigeon_direction.x)
         {
             if(desert.position.y > pigeon_direction.y && wasteland.position.y < pigeon_direction.y)
+            {
+                zone = "WASTELAND";
                 //score ++ for wasteland
+            }
+
+        }
+        else
+        {
+            zone = "nothing_here";
         }
                 Debug.Log(pigeon_direction);
     }
+
 
     private void OnTriggerEnter(Collider fly_pigeon)
     {
