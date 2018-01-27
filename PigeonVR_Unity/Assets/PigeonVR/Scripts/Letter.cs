4f2496b75a6679ac9a7c415745dd4bfe3af4e7f6
Ameen Radwan <therespie@gmail.com>
2018-01-27 13:20:00 -0500

    created letter with text in test scene

--- PigeonVR_Unity/Assets/PigeonVR/Scripts/Letter.cs
+++ PigeonVR_Unity/Assets/PigeonVR/Scripts/Letter.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using UnityEngine.UI;
 using UnityEngine;
 
-public class Letter : MonoBehaviour {
+public class Letter : MonoBehaviour
+{
 
     [SerializeField]
     Info.Location letterID;
@@ -12,20 +13,22 @@ public class Letter : MonoBehaviour {
     string displayDestination;
     Text displayText;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         displayText = GetComponentInChildren<Text>();
         letterID = Info.getRandomLocation();
         setStartData(letterID);
-	}
+    }
 
     void setStartData(Info.Location id)
     {
         this.displayDestination = Info.getRandomSender(id);
     }
-	
-	// Update is called once per frame
-	void Update () {
+
+    // Update is called once per frame
+    void Update()
+    {
         if (Input.GetKeyUp(KeyCode.Space))
         {
             Start();
