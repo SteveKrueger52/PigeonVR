b85067a9c85d7edf60053fa37060b3ad759fe2c7
SteveKrueger52 <stevekrueger52@gmail.com>
2018-01-26 21:30:41 -0500

    Added empty info singleton, will add json soon for initialization

---
+++ PigeonVR_Unity/Assets/PigeonVR/Scripts/Info.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Info : MonoBehaviour{
+    // Location Enumeration
+    public enum Location { TOWN, FOREST, DESERT, SNOW, VOLCANO, WASTELAND, CITY, MOON}
+
+    // Singleton Code
+    public static Info instance = null;
+    private Dictionary <Location, string[]> _senders = new Dictionary<Location, string> ();
+
+    private void Awake() {
+        // setup of Singleton
+        if (instance != null && instance != this) 
+        {
+            Destroy(this.gameObject);
+        }
+        instance = this;
+        DontDestroyOnLoad( this.gameObject );
+    }
+
+    // Public Functions
+    public static Info Instance {
+        get {
+            return instance ?? (instance = new GameObject("Database").AddComponent<Info>());
+        }
+    }
+
+    // Gets a list of all senders
+    public static string[] getAllSenders (Location loc) {
+        string[] output;
+        if (instance._senders.TryGetValue(loc,output))
+            return output;
+        return new string[]{};
+    }
+
+    // Gets a random sender
+    public static string getRandomSender (Location loc) {
+        string[] output;
+        if (instance._senders.TryGetValue(loc,output)) {
+            return output [(int) Random.Range (0, output.GetLength (0) - 1)];
+        }
+        return "Nobody In Particular";
+    }
+}
