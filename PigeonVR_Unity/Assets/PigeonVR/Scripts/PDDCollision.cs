857c266385a49c6607c6cc800cc0869b74abd2b6
GerritsC <gerrits.c@husky.neu.edu>
2018-01-27 22:50:20 -0500

    added alternate PDD technique

---
+++ PigeonVR_Unity/Assets/PigeonVR/Scripts/PDDCollision.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PDDCollision : MonoBehaviour {
+
+    [SerializeField]
+    Info.Location location;
+
+    private void OnCollisionExit(Collision collision)
+    {
+        var collided = collision.gameObject;
+
+        if (collided.tag == "Pigeon")
+        {
+            var pigeon = collided.GetComponent<Pigeon>();
+            //CHECKS FOR correct letter
+            if (pigeon._laden)
+            {
+                StateManager.Instance.updateAfterThrow(pigeon.letterLocation == this.location);
+            }
+
+            pigeon.StartCoroutine("GuessIllDie");
+        }
+    }
+}
